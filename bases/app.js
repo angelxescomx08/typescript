76409d84fca407099a4d2ab8d0e598c8d56b4654
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-05-23 19:41:21 -0600

    2.9 watcher

--- bases/app.js
+++ bases/app.js
@@ -1,3 +1,7 @@
 "use strict";
 let msg = "Hola mundo";
+const hero = {
+    name: 'Iron man',
+    age: 45
+};
 console.log(msg);
--- bases/app.ts
+++ bases/app.ts
@@ -1,2 +1,8 @@
 let msg: string = "Hola mundo"
+
+const hero = {
+    name: 'Iron man',
+    age: 45
+}
+
 console.log(msg);
