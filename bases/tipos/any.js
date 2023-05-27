0fffffb22951ca640b7ab9f78bbaf4169fe0dea0
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-05-26 19:48:58 -0600

    3.17 any

---
+++ bases/tipos/any.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    let avenger = 123;
+    console.log(avenger);
+    /**
+     * Se puede castear con el as
+     * avenger as number -------> se castea como número
+     *
+     * otra forma es
+     * <number>avenger
+     */
+})();
---
+++ bases/tipos/any.ts
@@ -0,0 +1,13 @@
+(() => {
+    let avenger: any = 123;
+
+    console.log(avenger);
+
+    /**
+     * Se puede castear con el as
+     * avenger as number -------> se castea como número
+     * 
+     * otra forma es 
+     * <number>avenger
+     */
+})()
