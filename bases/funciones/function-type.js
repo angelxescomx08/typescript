3955ac33be777a73a796e48c61eb45bbfaff41c0
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-06-04 19:37:59 -0600

    4.32 tipos funciones

---
+++ bases/funciones/function-type.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const addNumber = (a, b) => a + b;
+    const saveWorld = () => 'World saved!!';
+    const greed = (name) => `Hello ${name}`;
+    //let myFunction: Function;
+    let myFunction;
+})();
