833f9e8aee9ad06f094e5d2019263285522bf48f
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-06-04 18:02:40 -0600

    4.27 funciones basicas

---
+++ bases/funciones/functions.ts
@@ -0,0 +1,11 @@
+(() => {
+    const hero: string = 'flash'
+
+    function returnName(): string {
+        return hero;
+    }
+
+    const activateBatiSignam = () : string => {
+        return 'Activated'
+    }
+})()
