378725438d4771d14397874d03e100657df38f47
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-06-14 21:28:18 -0600

    publicos y privados

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -1,15 +1,28 @@
 (() => {
   class Avenger {
-    private name: string = "";
-    public team: string = "";
-    public realName?: string = "";
+    //private name: string = "";
+    //public team: string = "";
+    //public realName?: string = "";
 
     static avgAge: number = 35;
 
-    constructor(name: string, team: string, realName?: string){
+    /* constructor(name: string, team: string, realName?: string){
       this.name = name;
       this.team = team;
       this.realName = realName;
+    } */
+
+    /**Forma pro de declarar una propiedad y auto asignarla */
+    constructor(private name: string, private team: string, private realName?: string){
+
+    }
+
+    public bio(){
+
+    }
+
+    private bioPrivate(){
+
     }
   }
 
