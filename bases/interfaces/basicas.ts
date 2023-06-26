ab3a7fd38405a57ecf6f4a7747363edd6466393d
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-06-25 18:05:34 -0600

    interfaces

---
+++ bases/interfaces/basicas.ts
@@ -0,0 +1,17 @@
+(() => {
+  interface Hero {
+    name: string;
+    age: number;
+    getName?: () => string;
+  }
+
+  let flash: Hero = {
+    name: "Barry Alen",
+    age: 24,
+  };
+
+  let superman: Hero = {
+    name: "Clark",
+    age: 24,
+  };
+})();
