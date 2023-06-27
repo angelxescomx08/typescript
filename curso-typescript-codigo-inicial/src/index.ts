f26cf184cfceaa5bc9178423070bb0188f10b84e
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-06-26 20:48:32 -0600

    exportaciones por defecto

--- curso-typescript-codigo-inicial/src/index.ts
+++ curso-typescript-codigo-inicial/src/index.ts
@@ -1,5 +1,9 @@
-import { Hero } from './classes/hero'
+import { Hero, Hero1 as SuperHero } from "./classes/Hero";
+import * as Heros from "./classes/Hero";
+import powers, { Power } from "./data/powers";
 
 console.log("Hola Mundo!");
 
-const hero = new Hero('Spider-man',1,25);
+const hero = new Hero("Spider-man", 1, 25);
+
+console.log(powers);
